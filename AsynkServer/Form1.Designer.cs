b69f93de091f50705412e14357ed0b5ec0bc9d23
SalihMorina123 <146649620+SalihMorina123@users.noreply.github.com>
2024-05-30 22:09:55 +0200

    Add files via upload

---
+++ AsynkServer/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿
+namespace AsynkServer
+{
+    partial class Form1
+    {
+        
+        private System.ComponentModel.IContainer components = null;
+
+       
+      
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+   
+        private void InitializeComponent()
+        {
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.buttonStartServer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(175, 108);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxLog.Size = new System.Drawing.Size(400, 200);
+            this.textBoxLog.TabIndex = 0;
+            // 
+            // buttonStartServer
+            // 
+            this.buttonStartServer.Location = new System.Drawing.Point(349, 52);
+            this.buttonStartServer.Name = "buttonStartServer";
+            this.buttonStartServer.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartServer.TabIndex = 1;
+            this.buttonStartServer.Text = "Start Server";
+            this.buttonStartServer.UseVisualStyleBackColor = true;
+            this.buttonStartServer.Click += new System.EventHandler(this.buttonStartServer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStartServer);
+            this.Controls.Add(this.textBoxLog);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.Button buttonStartServer;
+    }
+}
+
---
+++ AsynkServer/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsynkServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void buttonStartServer_Click(object sender, EventArgs e)
+        {
+            TcpListener server = new TcpListener(IPAddress.Any, 5000);
+            server.Start();
+            textBoxLog.AppendText("Server started...\r\n");
+
+            while (true)
+            {
+                try
+                {
+                    TcpClient client = await server.AcceptTcpClientAsync();
+                    textBoxLog.AppendText("Client connected...\r\n");
+
+                    _ = Task.Run(async () =>
+                    {
+                        try
+                        {
+                            using (NetworkStream stream = client.GetStream())
+                            {
+                                byte[] buffer = new byte[1024];
+                                int bytesRead;
+                                while ((bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length)) != 0)
+                                {
+                                    string message = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+                                    Invoke((Action)(() => textBoxLog.AppendText($"Received: {message}\r\n")));
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Invoke((Action)(() => textBoxLog.AppendText($"Error: {ex.Message}\r\n")));
+                        }
+                        textBoxLog.AppendText("Client disconnected...\r\n");
+                    });
+                }
+                catch (Exception ex)
+                {
+                    textBoxLog.AppendText($"Error: {ex.Message}\r\n");
+                }
+            }
+        }
+    }
+}
